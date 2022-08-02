76eb7e877a07ec4fd698e844005f5ade3456ef09
Sam Rodrigues <sam@olivers.tech>
2022-08-01 22:52:25 -0300

    Add spinner

--- resources/js/AppVote.vue
+++ resources/js/AppVote.vue
@@ -24,7 +24,17 @@
     </fieldset>
 
     <div class="mt-5">
-      <button @click="sendVote" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" :disabled="loading || !vote.length">Confirmar</button>
+      <button @click="sendVote"
+              class="w-full flex justify-center py-2 px-4
+              border border-transparent rounded-md shadow-sm
+              text-sm font-medium text-white bg-emerald-600
+              hover:bg-emerald-700 focus:outline-none
+              focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+              :disabled="loading || !vote.length"><template v-if="loading"><svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-slate-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg></template><template v-else>Confirmar</template></button>
     </div>
   </div>
 </template>
@@ -64,7 +74,7 @@ export default {
         voteList = voteList.filter(v => !pollIds.includes(v));
       })
       voteList = voteList.map(v => pollTitles.includes(v) ? 0 : v);
-      return
+
       const vote = {
         hash: hash.MD5(`${name}${dob}`),
         voteList
