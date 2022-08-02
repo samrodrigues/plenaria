b963b0a02f208bb2464b7e7cd4b12ee8490b8473
Sam Rodrigues <sam@olivers.tech>
2022-08-02 11:17:32 -0300

    Improve styling

--- resources/js/TheHeader.vue
+++ resources/js/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="text-center pb-5">
-        <h1 class="text-xl bold">{{ title }}</h1>
-        <h2 class="text-md">{{ location }}</h2>
-        <h3 class="text-sm">{{ timeString }}</h3>
+        <h1 class="text-xl bold text-gray-900">{{ title }}</h1>
+        <h2 class="text-md text-gray-700">{{ location }}</h2>
+        <h3 class="text-sm text-gray-700">{{ timeString }}</h3>
         <hr class="mt-5" />
     </div>
 </template>
